b3e943622e0731159858bfd798b5a69d0c07c66d
MuhammadqodirProgrammer <komilovm831@gmail.com>
2023-07-23 12:24:17 +0500

    simple

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import { useTelegram } from '../hooks/useTelegram';
 
@@ -7,6 +8,11 @@ const Header = () => {
 	return (
 		<div className={'header'}>
 			<Button onClick={onClose}>close</Button>
+<br />
+<Link to="/">home</Link>
+<br />
+<br />
+<Link to="/form">form</Link>
 
 			<span className='username'>{user?.username}</span>
 		</div>
